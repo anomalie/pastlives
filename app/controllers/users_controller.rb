8c15925c0d15777dd5f75a8e723e6921f0699fcf
Emilie Corpuz <emiliecorpuz@gmail.com>
2015-04-27 18:05:24 -0700

    herkou test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:success] = ["You've successfully created an account"]
     
       redirect_to('/')
